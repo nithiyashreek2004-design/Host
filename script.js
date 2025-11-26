8780ac470cac533ee83ea1bb4501ae5869b22084
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-11-26 10:02:44 +0530

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1. form validation
+// 2.annimaTIONS
+// 3.POPUPS
+// 4.API request
+// 5.CHANGINGING CONTENT DYNAMICALLY
+// 6.bukding full application(React,angular,vue)
+
+// variable name
+// let
+// const
+// var
