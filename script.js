cf66ad6c3b554230dbc4e7e197ae0b1ceb22ab08
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-11-28 09:11:23 +0530

    update

--- script.js
+++ script.js
@@ -10,16 +10,19 @@
 // const
 // var
 
-variable and Constant 
-var=NAME THE REFERENCE THAT POINT TO  values stored in memory 
-js variable stored  not the value themselves
-primitive value like string number boolean null undefined begind store the actuall value
-non premitive object ,array function stored the reference to the location in memory where the value is stored
- var can be redeclared is allowed and reassign var will ignore the block scope
-let can be reassigned but not redeclared
-const cannot be redeclared or reassigned
-let is propersties morden varoiable declaration let is an block scoped can be reassignbut not declared in same scope recommented most variables
-global scope=not inside the block 
- local scope ={}inside the block 
- cont constant varable it is block scoped cant be reassiend must be initialized when declared good for  value never change 
- 
+// variable and Constant 
+// var=NAME THE REFERENCE THAT POINT TO  values stored in memory 
+// js variable stored  not the value themselves
+// primitive value like string number boolean null undefined begind store the actuall value
+// non premitive object ,array function stored the reference to the location in memory where the value is stored
+//  var can be redeclared is allowed and reassign var will ignore the block scope
+// let can be reassigned but not redeclared
+// const cannot be redeclared or reassigned
+// let is propersties morden varoiable declaration let is an block scoped can be reassignbut not declared in same scope recommented most variables
+// global scope=not inside the block 
+//  local scope ={}inside the block 
+//  cont constant varable it is block scoped cant be reassiend must be initialized when declared good for  value never change 
+//  collection of information is called data 2 type primitive  and non primitive
+//  seven primitive -7 non primitive=1
+
+
