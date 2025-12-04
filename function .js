a01dc1aecdb9a12b279cb45fb2fd0a23277481b4
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2025-12-04 16:45:15 +0530

    update

---
+++ function .js
@@ -0,0 +1,18 @@
+//definition func a bock of code defined a paeiticular code its executed when somthishg invokes it calls it 
+// a func run only whtn u call it
+//it receive input parameeter and return a single out put return vaue
+//key points 
+//reusable code
+//reduce code redundancy
+// makes logic and organization better
+//enables modular programming
+//syntax
+//function functionName (parameter1, parameter2){
+//code to be executed
+//}
+//}
+//function call
+//function declaration
+function sum(a, b) {
+  return a + b;
+}
