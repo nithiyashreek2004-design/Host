19b6d12035beb2f9d05bc7143d20c1693fda5fe6
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-01-23 13:13:14 +0530

    update

---
+++ day 1.sql/day1.sql
@@ -0,0 +1,6 @@
+-- sql uaed to store retrive update and delete data work swith all database like my sql post gerst sql oracle sql server 
+
+y need
+--   ues in web mobile app banking and hospital  database is collection of table each table has row abd column 
+
+db is organized collection of data  stored as electornically 
